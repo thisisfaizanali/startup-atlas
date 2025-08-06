dbc566b747b3e4c71fb55920eb344fc43cc896a8
thisisfaizanali <faizanaliwhb@gmail.com>
2025-08-06 18:57:46 +0530

    Implement Navbar component with logo and styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import React from "react";
-
+import Image from "next/image";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
