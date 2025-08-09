d0407e5c95520d6bb92694cee16e83fb92f2a304
thisisfaizanali <faizanaliwhb@gmail.com>
2025-08-09 18:35:24 +0530

    Refactor Navbar component to include authentication logic and user session handling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,37 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-const Navbar = () => {
+import { auth, signIn, signOut } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <button onClick={signOut}>
+                <span>Logout</span>
+              </button>
+
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <button onClick={signIn}>
+              <span>Login</span>
+            </button>
+          )}
+        </div>
       </nav>
     </header>
   );
