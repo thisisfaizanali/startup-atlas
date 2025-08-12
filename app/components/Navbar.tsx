a0b2e27b976a6c18eb28ed74ec0af1aaba0ef248
thisisfaizanali <faizanaliwhb@gmail.com>
2025-08-12 23:48:28 +0530

    Refactor Navbar component for consistent import statement formatting and improve logout/login button handling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { auth, signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from '@/auth';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = async () => {
   const session = await auth();
@@ -11,25 +10,35 @@ const Navbar = async () => {
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
                 <span>Create</span>
               </Link>
 
-              <button onClick={signOut}>
-                <span>Logout</span>
-              </button>
+              <form
+                action={async () => {
+                  'use server';
+                  await signOut({ redirectTo: '/' });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
 
-              <Link href={`/user/${session?.id}`}>
+              <Link href={`/user/${session?.user?.id}`}>
                 <span>{session?.user?.name}</span>
               </Link>
             </>
           ) : (
-            <button onClick={signIn}>
-              <span>Login</span>
-            </button>
+            <form
+              action={async () => {
+                'use server';
+                await signIn('github');
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
           )}
         </div>
       </nav>
