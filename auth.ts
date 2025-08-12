99694d96f3970fee85c1fe3adff16b2d4345c7ef
thisisfaizanali <faizanaliwhb@gmail.com>
2025-08-12 23:47:43 +0530

    Add header comment to auth.ts for clarity

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+// auth.ts
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
