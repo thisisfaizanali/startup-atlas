ef6cd60cd42b610c84f1608a8145fb75c39d0206
thisisfaizanali <faizanaliwhb@gmail.com>
2025-08-13 23:58:54 +0530

    feat : add the heroes section for the website

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 const Home = () => {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup Idea, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
     </>
   );
 };
