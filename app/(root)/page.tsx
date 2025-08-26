45fbbb1ce96e817a317356576d5de89ada17e38d
thisisfaizanali <faizanaliwhb@gmail.com>
2025-08-26 20:44:55 +0530

    feat: add StartupCard component and implement posts display in Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 
 const Home = async ({
@@ -6,6 +7,20 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Faizan' },
+      _id: 1,
+      description: 'This is a description',
+      image:
+        'https://plus.unsplash.com/premium_photo-1682464708085-95b4486e2c32?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      category: 'robots',
+      title: 'We robots',
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -19,6 +34,22 @@ const Home = async ({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? `Search results for $query` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString('en-us', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+}
