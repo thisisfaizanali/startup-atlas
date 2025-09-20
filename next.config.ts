49a981db0d85921af52ab15d244c24817c48af33
thisisfaizanali <faizanaliwhb@gmail.com>
2025-09-21 00:14:25 +0530

    chore: update dependencies and improve TypeScript configuration
    - Updated Next.js version to ^15.6.0-canary.7 in package.json.
    - Enhanced the STARTUPS_QUERY in queries.ts for better readability and added STARTUP_BY_ID_QUERY for fetching a startup by ID.
    - Modified tsconfig.json to use "react-jsx" for JSX transformation and improved formatting for better clarity.

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  experimental: {
+    ppr: 'incremental',
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: 'bottom-right',
+  },
 };
 
 export default nextConfig;
